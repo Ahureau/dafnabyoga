1e784dac35b1f13d4faab57b4a392fbfbf3fbc55
AlexH <53427235+Ahureau@users.noreply.github.com>
2024-09-09 16:41:15 +0200

    Bugged header template added
    For some reason it's showing up twice, but there's now a header template and it works well.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles/main.css";
+
+import { addHeader } from './header.js';
+
+const container = document.querySelector('#maxWidthContainer');
+
+addHeader(container);
--- webpack.config.js
+++ webpack.config.js
@@ -83,5 +83,6 @@ module.exports = {
       },
     },
     hot: true,
+    watchFiles: ['src/**/*'],
   },
 };
