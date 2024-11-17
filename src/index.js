2f592cb8fa87ea7acf521b6442e327f180c16ab4
AlexH <53427235+Ahureau@users.noreply.github.com>
2024-11-17 11:54:02 +0100

    mailing list js test

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ addHeader(body);
 addMailingSection(body);
 addFooter(body);
 
+// Mobile menu event listener
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.getElementById('hamburgerButton');
     const headerLinks = document.querySelector('.headerLinks');
@@ -28,6 +29,37 @@ document.addEventListener('DOMContentLoaded', () => {
         headerLinks.classList.remove('active');
     });
   });
+
+
+// Mailing list js
+document.getElementById('newsletterEmailForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(event.target);
+    const email = formData.get('email');
+
+    fetch('https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec', {
+        method: 'POST',
+        body: new URLSearchParams({
+            email: email,
+            Date: new Date().toISOString() // Add the current date
+        }),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === 'success') {
+            document.querySelector('output[name="confirmation"]').style.display = 'block';
+        } else {
+            alert('Error: ' + data.error);
+        }
+    })
+    .catch(error => {
+        alert('Error: ' + error);
+    });
+});
   
   
 
