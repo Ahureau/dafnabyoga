5f18856c0b21efa91b0d8c8933c81134c104e8f1
AlexH <53427235+Ahureau@users.noreply.github.com>
2024-11-17 11:43:19 +0100

    new test at email js

--- src/index.js
+++ src/index.js
@@ -28,31 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
         headerLinks.classList.remove('active');
     });
   });
-
-
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbxGzFYRbOIv6wUkb8fXUCQeHhyg2ORtn5F0iuM3uy8J6HjUNEHLslHwFW45hZESw_kSfA/exec';
-  const form = document.querySelector('#form');
-  const btn = document.querySelector('#submit');
-  
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    btn.disabled = true;
-    btn.innerHTML = "Loading...";
-  
-    console.log(form);
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then(response => {
-        btn.disabled = false;
-        btn.innerHTML = "Submit";
-        alert('Success!', response);
-      })
-      .catch(error => {
-        btn.disabled = false;
-        btn.innerHTML = "Submit";
-        alert('Error!', error.message);
-      });
-  });
   
   
 
